b86c91d1e1de83676e54d9b08d3ad7d99dca7506
thuavanan2004 <thuavanan2004@gmail.com>
2024-07-12 21:41:34 +0700

    complete layout page tours

--- public/client/js/script.js
+++ public/client/js/script.js
@@ -8,3 +8,31 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 // End Form Search Date 
+// Slider 
+const priceSlider = document.getElementById('price-slider');
+const minPriceValue = document.getElementById('min-price-value');
+const maxPriceValue = document.getElementById('max-price-value');
+
+noUiSlider.create(priceSlider, {
+  start: [0, 1000], // Giá trị ban đầu
+  connect: true,
+  range: {
+    'min': 0,
+    'max': 1000
+  },
+  step: 1,
+  format: {
+    to: value => Math.round(value),
+    from: value => Number(value)
+  }
+});
+
+// Cập nhật giá trị khi thanh trượt thay đổi
+priceSlider.noUiSlider.on('update', (values, handle) => {
+  if (handle === 0) {
+    minPriceValue.textContent = values[0];
+  } else {
+    maxPriceValue.textContent = values[1];
+  }
+});
+// End Slider 
